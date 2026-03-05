be9c7574f394089fa27be54a9224050447d326de
Jaime Arenal Rocha <jaimearenalrocha@gmail.com>
2026-03-05 20:15:29 +0100

    mejoras eestilo

--- frontend/src/pages/GameMode.jsx
+++ frontend/src/pages/GameMode.jsx
@@ -9,12 +9,11 @@ const GameMode = () => {
       <HeaderComponent></HeaderComponent>
       <h2>MODOS DE JUEGO</h2>
       <p>
-        ¡Prepárate para el desafío definitivo! Pon a prueba tu agilidad mental y
-        tu vocabulario completando el famoso Rosco
+        ¡Prepárate para el desafío definitivo! <br />Pon a prueba tu agilidad mental y
+        tu vocabulario completando el famoso <br /><br />
       </p>
       <section>
         <p>
-          Aquí puedes empezar a añadir el contenido principal de tu aplicación.
         </p>
         <GameModeComponent></GameModeComponent>
       </section>
